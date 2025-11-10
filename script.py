abab1443c11313dcc8e574c1e98d56ede3d2c09d
Jjangpongg <leanlasay@gmail.com>
2025-11-10 06:04:39 +0000

    added get_balance for later

--- script.py
+++ script.py
@@ -11,6 +11,11 @@ class AVLTree:
             return 0
         return root.height
     
+    def get_balance(self, root):
+        if not root:
+            return 0
+        return self.get_height(root.left) - self.get_height(root.right)
+    
         
 
 
